72d2721ff6023d9a2499f5757dec806231c14fb6
Josh Eklund <Josh Eklund>
2016-02-04 23:54:50 -0500

    Updated script1 to contain the new pin list which will have all active low pins on startup and corresponds to GPIO 0-7 respectively.

--- 8port/script1.py
+++ 8port/script1.py
@@ -6,7 +6,12 @@ GPIO.setmode(GPIO.BCM)
 
 # init list with pin numbers
 
-pinList = [2, 3, 4, 17, 27, 22, 10, 9]
+#pinList = [2, 3, 4, 17, 27, 22, 10, 9]
+
+# New Pin List According to WiringPi GPIO 0-7
+# These pins will all be Active Low & IN on Boot
+# according to the RPI Rev 2 Board Specs
+pinList = [17, 18, 27, 22, 23, 24, 25, 4]
 
 # loop through pins and set mode and state to 'low'
 
@@ -21,28 +26,28 @@ SleepTimeL = 2
 # main loop
 
 try:
-  GPIO.output(2, GPIO.LOW)
+  GPIO.output(17, GPIO.LOW)
   print "ONE"
   time.sleep(SleepTimeL); 
-  GPIO.output(3, GPIO.LOW)
+  GPIO.output(18, GPIO.LOW)
   print "TWO"
   time.sleep(SleepTimeL);  
-  GPIO.output(4, GPIO.LOW)
+  GPIO.output(27, GPIO.LOW)
   print "THREE"
   time.sleep(SleepTimeL);
-  GPIO.output(17, GPIO.LOW)
+  GPIO.output(22, GPIO.LOW)
   print "FOUR"
   time.sleep(SleepTimeL);
-  GPIO.output(27, GPIO.LOW)
+  GPIO.output(23, GPIO.LOW)
   print "FIVE"
   time.sleep(SleepTimeL);
-  GPIO.output(22, GPIO.LOW)
+  GPIO.output(24, GPIO.LOW)
   print "SIX"
   time.sleep(SleepTimeL);
-  GPIO.output(10, GPIO.LOW)
+  GPIO.output(25, GPIO.LOW)
   print "SEVEN"
   time.sleep(SleepTimeL);
-  GPIO.output(9, GPIO.LOW)
+  GPIO.output(4, GPIO.LOW)
   print "EIGHT"
   time.sleep(SleepTimeL);
   GPIO.cleanup()
